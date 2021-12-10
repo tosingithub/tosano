6ec034336f53394c3250f2c39a87d35d7dada14d
oluwatosin adewumi <oluade@dgx01.north.sics.se>
2021-12-10 15:50:48 +0100

    First upload to new branch

--- ro.py
+++ ro.py
@@ -36,7 +36,7 @@ args = parser.parse_args()
 if __name__=="__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    data, t_data = util.read_data()
+    data, test_data = util.read_data()
     print(data.head())
 
     # tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
