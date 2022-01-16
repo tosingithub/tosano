4508b3194085f3a9a68af519f7d70c7a52262d9b
oluwatosin adewumi <oluade@dgx01.north.sics.se>
2022-01-16 20:11:56 +0100

    updates to T5 code

--- utility_hs.py
+++ utility_hs.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import argparse
 import re
+from sklearn.model_selection import train_test_split
 
 
 parser = argparse.ArgumentParser(description='Hate Speech')
 # --datatype: hasoc, trol, sema, semb, hos, olid (--olidtask = a, b or c)
 # USAGE EXAMPLE in the terminal: python ht5.py --datatype trol
-parser.add_argument('--datatype', type=str, default='hasoc', help='data of choice')
+parser.add_argument('--datatype', type=str, default='olid', help='data of choice')
 parser.add_argument('--has19_traindata', type=str, default='/home/shared_data/h/has19_traindata.csv', help='location of the training data')
 parser.add_argument('--has19_devdata', type=str, default='/home/shared_data/h/has19_devdata.csv', help='location of the dev data')
 parser.add_argument('--has19_testdata', type=str, default='/home/shared_data/h/has19_testdata.csv', help='location of the test data')
@@ -31,15 +32,21 @@ parser.add_argument('--hos_devdata', type=str, default='/home/shared_data/h/HOS_
 # OLID
 parser.add_argument('--olid_traindata', type=str, default='/home/shared_data/h/OLIDv1.0/olid-training.csv', help='location of the training data')
 parser.add_argument('--olid_devdata', type=str, default='/home/shared_data/h/OLIDv1.0/olid-dev.csv', help='location of the dev data')
-parser.add_argument('--olidtask', type=str, default="a", help='Task Alphabet')      # a, b or c
+# Augmented Training Data
+parser.add_argument('--has20_aug_data', type=str, default='/home/shared_data/h/hasoc2020_drop_augmented_train.csv', help='location of the augmented training data')
+parser.add_argument('--has21_aug_data', type=str, default='/home/shared_data/h/hasoc2021_drop_augmented_train.csv', help='location of the augmented training data')
+parser.add_argument('--has20_aug_gen', type=str, default='/home/shared_data/h/has20_gen_augmented_train.csv', help='location of the augmented training data')
+parser.add_argument('--has21_aug_gen', type=str, default='/home/shared_data/h/has21_gen_augmented_train.csv', help='location of the augmented training data')
+
+#tdata21_new.to_csv('/home/shared_data/h/has21_gen_augmented_train.csv', index=False)
 
 args = parser.parse_args()
 
 
-def get_data(datatype, datayear='2020', combined_traindata=False):
+def get_data(datatype, datayear='2020', task=2, augment_traindata=False):
     """ Select the dataset to use """
     if datatype == 'hasoc':
-        if not combined_traindata:
+        if not augment_traindata:
             if datayear == '2020':
                 print('Using Hasoc 2020 data... ')
                 traindata = pd.read_csv(args.has20_traindata)
@@ -51,19 +58,28 @@ def get_data(datatype, datayear='2020', combined_traindata=False):
                 devdata = pd.read_csv(args.has21_devdata)
                 testdata = pd.read_csv(args.has21_testdata)
         else:
-            data1a = pd.read_csv(args.has19_traindata)
-            data2b = pd.read_csv(args.has19_devdata)
-            data1aa = pd.read_csv(args.has20_traindata)
-            data2bb = pd.read_csv(args.has20_devdata)
-            data1aaa = pd.read_csv(args.has21_traindata)
-            data2bbb = pd.read_csv(args.has21_devdata)
-            traindata, devdata = pd.concat([data1a, data1aa, data1aaa]), pd.concat([data2b, data2bb, data2bbb])
             if datayear == '2020':
-                print('Using Hasoc combined data for Hasoc 2020 test data... ')
+                print('Using Hasoc augmented data for Hasoc 2020 test data... ')
+                data20a = pd.read_csv(args.has20_traindata)
+                data20b = pd.read_csv(args.has20_aug_data)
+                traindata = pd.concat([data20a, data20b])
+                devdata = pd.read_csv(args.has20_devdata)
                 testdata = pd.read_csv(args.has20_testdata)
             else:
-                print('Using Hasoc combined data for Hasoc 2021 test data... ')
-                testdata = pd.read_csv(args.has21_testdata)     
+                print('Using Hasoc augmented data for Hasoc 2021 test data... ')
+                data21a = pd.read_csv(args.has21_traindata)
+                data21b = pd.read_csv(args.has21_aug_gen)
+                devdata = pd.read_csv(args.has21_devdata)
+                # data21c = pd.read_csv(args.has21_aug_data)
+                traindata = pd.concat([data21a, data21b])
+                # re-jig validation split
+                # if task=='1':
+                # X_train, X_val = train_test_split(traindata, test_size=0.10, shuffle=True) #, random_state=42, stratify=traindata['task_1'].values)
+                # else:
+                #     X_train, X_val = train_test_split(traindata, test_size=0.10, random_state=42, stratify=traindata['task_2'].values)
+                # traindata = X_train
+                # devdata = X_val
+                testdata = pd.read_csv(args.has21_testdata)
     elif datatype =='sema':
         print('Using HatEval SemEval_A data... ')
         traindata = pd.read_csv(args.sema_traindata, sep='\t', header=0, encoding="latin1").fillna(method="ffill")
@@ -114,7 +130,7 @@ def preprocess_pandas(data, columns):
         df_['Text'] = data['Text'].str.strip()
         df_['Text'] = data['Text'].replace('\d', '', regex=True)                                                   # remove numbers
         df_.drop_duplicates(subset=['Text'], keep='first')
-        df_.dropna()
+        df_ = df_.dropna()
     elif args.datatype == 'hos' or args.datatype == 'olid':
         df_['tweet'] = data['tweet'].replace('[a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+', '', regex=True)                      # remove emails
         df_['tweet'] = data['tweet'].replace('((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}', '', regex=True)    # remove IP address
@@ -130,7 +146,7 @@ def preprocess_pandas(data, columns):
         df_['tweet'] = data['tweet'].str.strip()
         df_['tweet'] = data['tweet'].replace('\d', '', regex=True)                                                   # remove numbers
         df_.drop_duplicates(subset=['tweet'], keep='first')
-        df_.dropna()
+        df_ = df_.dropna()
     else:               #args.datatype == 'hasoc' or args.datatype == 'sema' or args.datatype == 'semb':
         df_['text'] = data['text'].replace('[a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+', '', regex=True)                      # remove emails
         df_['text'] = data['text'].replace('((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}', '', regex=True)    # remove IP address
@@ -146,6 +162,6 @@ def preprocess_pandas(data, columns):
         df_['text'] = data['text'].str.strip()
         df_['text'] = data['text'].replace('\d', '', regex=True)                                                   # remove numbers
         df_.drop_duplicates(subset=['text'], keep='first')
-        df_.dropna()
+        df_ = df_.dropna()
 
     return df_
